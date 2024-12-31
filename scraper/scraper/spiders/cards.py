ef39f3307de9895505f0f474871f80bcbbfcd4f6
Owen B <owen.b@example.com>
2024-12-31 02:52:03 -0500

    Work done on card scraping

--- scraper/scraper/spiders/cards.py
+++ scraper/scraper/spiders/cards.py
@@ -32,3 +32,47 @@ class CardsSpider(scrapy.Spider):
                 "link" : url,
             }
 
+            yield response.follow(url, self.parse_card)
+
+    def parse_card (self, response):
+
+        name_eng = response.xpath(".//tr/td[text() = 'Name']/following-sibling::td/*/text()").get()
+        name_jap = response.xpath(".//tr/td[text() = 'Japanese']/following-sibling::td/text()").get().replace('\n', '')
+        name_chi = response.xpath(".//tr/td[text() = 'Traditional Chinese']/following-sibling::td/text()").get().replace('\n', '')
+        colour = response.xpath(".//tr/td[text() = 'Colour']/following-sibling::td/*/text()").getall()
+
+        card_type = response.xpath(".//tr/td[text() = 'Card Type']/following-sibling::td/*/text()").get()
+
+        level = response.xpath(".//tr/td[text() = 'Level']/following-sibling::td/*/text()").get()
+        form = response.xpath(".//tr/td[text() = 'Form']/following-sibling::td/*/text()").get()
+        type = response.xpath(".//tr/td[text() = 'Type']/following-sibling::td/*/text()").getall()
+        rarity  = response.xpath(".//tr/td[text() = 'Rarity']/following-sibling::td/*/text()").get()
+        evo_conditions = list()
+
+        # evo_con_elements = response.css (".evocon").getall()
+        # evo_con_elements = [i for i in evo_con_elements if '\n' not in i]
+
+        # for evo_con in evo_con_elements:
+        #     evo_colour = evo_con.css ("tbody > tr > td")
+
+        #     evo_conditions.append({"colour": })
+
+
+        self.log ("Parsing card {} with id {}".format(name_eng, 0 ))
+
+        yield {
+            "name_eng": name_eng,
+            "name_jap": name_jap,
+            "name_chi": name_chi,
+            "colour": colour,
+            "card_type": card_type,
+            "level": level,
+            "form": form,
+            "type": type,
+            "rarity": rarity,
+        }
+        
+
+        
+
+             
